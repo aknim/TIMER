a6ae1c83ff6d9ab24ee59fea3ce131b382e4a2d0
Akshaya <akshayasunny@gmail.com>
2014-03-18 16:52:14 +0530

    made installation location independent

--- helper/installer.sh
+++ helper/installer.sh
@@ -1,7 +1,11 @@
 echo "WELCOME TO THE TIMER APP!";
-echo "Enter the directory for installation";
-read location;
 
-cp -r ../scripts ${location}/;
+echo "Enter the downloaded directory location [location of timer/scripts folder]";
+read location_dwnld;
+
+echo "Enter the directory for installation";
+read location_instl;
+
+cp -r ${location_dwnld}/timer/scripts ${location_instl}/;
 mkdir -p /tmp/timer/;
 
